b40e425b832fccc17c279a83d002a74b9e018c9f
ahmed sohel <ms19sohel@gmail.com>
2021-11-14 19:30:02 +0600

    starting the project

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav-link');
+const crossIcon = document.querySelector('.cross-icon')
+
+if(hamburger) {
+    hamburger.addEventListener('click', (e) => {
+        navLink.style.display = 'block';
+    });
+};
+
+if(crossIcon) {
+    crossIcon.addEventListener('click', (a) => {
+        navLink.style.display = 'none';
+
+    })
+}
